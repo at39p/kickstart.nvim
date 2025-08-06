3ba4a448e9b752ba2c6078f5f5f8ed6f692b7710
at39p <superdnrs@protonmail.com>
2025-08-06 23:50:50 +0200

    Improved theme for Go

--- colors/macostheme.lua
+++ colors/macostheme.lua
@@ -33,15 +33,16 @@ local colors = {
   inc_search = '#ffce77',
 
   -- Syntax colors (from Zed macOS Classic)
-  keyword = '#0000a2', -- Dark blue for keywords (pub, const, fn, var, if, return)
+  keyword = '#0000a2', -- Dark blue for keywords (pub, const, fn, func, var, if, return)
   type_color = '#6e79f1', -- Purple for types and type names
-  property = '#c5060b', -- Red for properties, fields, enum members
+  field = '#c5060b', -- Red for struct fields, enum members, object keys
   string = '#036a07', -- Dark green for strings
   number = '#0000cd', -- Blue for numbers
   comment = '#0066ff', -- Light blue for comments
-  constant = '#c5060b', -- Red for constants (uppercase)
-  function_call = '#000000', -- Black for function calls
-  namespace = '#000000', -- Black for namespaces
+  constant = '#c5060b', -- Red for constants (uppercase identifiers)
+  variable = '#000000', -- Black for variables and parameters
+  function_name = '#000000', -- Black for function names and calls
+  namespace = '#000000', -- Black for namespaces/packages
   operator = '#000000', -- Black for operators
 
   -- Additional colors
@@ -50,9 +51,9 @@ local colors = {
   info = '#4169e1',
   hint = '#808080',
 
-  -- Git colors
-  git_add = '#22863a',
-  git_change = '#e36209',
+  -- Git colors (matching Zed file explorer)
+  git_add = '#0000a2', -- Blue for new files (matches keyword blue)
+  git_change = '#c57c41', -- Muted orange for modified files
   git_delete = '#d73a49',
 }
 
@@ -84,7 +85,7 @@ local highlights = {
   SignColumn = { fg = colors.fg_tertiary, bg = colors.bg },
   IncSearch = { bg = colors.inc_search },
   Search = { bg = colors.search_bg },
-  Substitute = { fg = colors.bg, bg = colors.property },
+  Substitute = { fg = colors.bg, bg = colors.field },
   MatchParen = { bg = colors.selection, bold = true },
   ModeMsg = { fg = colors.fg_secondary },
   MsgArea = { fg = colors.fg },
@@ -116,18 +117,18 @@ local highlights = {
 
   -- Syntax highlights
   Comment = { fg = colors.comment, italic = true },
-  Constant = { fg = colors.type_color }, -- null, true, false
+  Constant = { fg = colors.type_color }, -- nil, true, false should be purple
   String = { fg = colors.string },
   Character = { fg = colors.string },
   Number = { fg = colors.number },
-  Boolean = { fg = colors.type_color }, -- true/false like null
+  Boolean = { fg = colors.type_color }, -- true/false like nil
   Float = { fg = colors.number },
-  Identifier = { fg = colors.property }, -- Variable names, field names
-  Function = { fg = colors.function_call }, -- Function names in definitions and calls
+  Identifier = { fg = colors.variable }, -- Regular identifiers should be black
+  Function = { fg = colors.function_name }, -- Function names should be black
   Statement = { fg = colors.keyword }, -- Keywords
   Conditional = { fg = colors.keyword }, -- if, else
   Repeat = { fg = colors.keyword }, -- for, while
-  Label = { fg = colors.property },
+  Label = { fg = colors.field }, -- Labels can be red
   Operator = { fg = colors.operator },
   Keyword = { fg = colors.keyword },
   Exception = { fg = colors.keyword },
@@ -136,16 +137,16 @@ local highlights = {
   Define = { fg = colors.keyword },
   Macro = { fg = colors.constant },
   PreCondit = { fg = colors.keyword },
-  Type = { fg = colors.type_color }, -- Type names
+  Type = { fg = colors.type_color }, -- Type names should be purple
   StorageClass = { fg = colors.keyword }, -- const, var, pub
   Structure = { fg = colors.keyword }, -- struct, enum
   Typedef = { fg = colors.type_color },
-  Special = { fg = colors.fg },
+  Special = { fg = colors.variable },
   SpecialChar = { fg = colors.string },
   Tag = { fg = colors.keyword },
   Delimiter = { fg = colors.fg },
   SpecialComment = { fg = colors.comment, italic = true },
-  Debug = { fg = colors.property },
+  Debug = { fg = colors.field },
   Underlined = { underline = true },
   Ignore = { fg = colors.fg_tertiary },
   Error = { fg = colors.error },
@@ -176,45 +177,45 @@ local highlights = {
   -- TreeSitter highlights for accurate syntax
   ['@annotation'] = { fg = colors.keyword },
   ['@attribute'] = { fg = colors.keyword },
-  ['@boolean'] = { fg = colors.type_color }, -- true/false like types
+  ['@boolean'] = { fg = colors.type_color }, -- true/false like nil
   ['@character'] = { fg = colors.string },
   ['@character.special'] = { fg = colors.string },
   ['@comment'] = { fg = colors.comment, italic = true },
   ['@conditional'] = { fg = colors.keyword },
   ['@constant'] = { fg = colors.constant }, -- UPPERCASE constants
-  ['@constant.builtin'] = { fg = colors.type_color }, -- null, undefined
+  ['@constant.builtin'] = { fg = colors.type_color }, -- nil, undefined
   ['@constant.macro'] = { fg = colors.constant },
   ['@constructor'] = { fg = colors.type_color },
-  ['@debug'] = { fg = colors.property },
+  ['@debug'] = { fg = colors.field },
   ['@define'] = { fg = colors.keyword },
   ['@error'] = { fg = colors.error },
   ['@exception'] = { fg = colors.keyword },
-  ['@field'] = { fg = colors.property }, -- Field names
+  ['@field'] = { fg = colors.field }, -- Struct/object field declarations
   ['@float'] = { fg = colors.number },
-  ['@function'] = { fg = colors.function_call },
-  ['@function.builtin'] = { fg = colors.function_call },
-  ['@function.call'] = { fg = colors.function_call },
+  ['@function'] = { fg = colors.function_name },
+  ['@function.builtin'] = { fg = colors.function_name },
+  ['@function.call'] = { fg = colors.function_name },
   ['@function.macro'] = { fg = colors.keyword },
-  ['@function.method'] = { fg = colors.function_call },
-  ['@function.method.call'] = { fg = colors.function_call },
+  ['@function.method'] = { fg = colors.function_name },
+  ['@function.method.call'] = { fg = colors.function_name },
   ['@include'] = { fg = colors.keyword },
   ['@keyword'] = { fg = colors.keyword },
   ['@keyword.coroutine'] = { fg = colors.keyword },
-  ['@keyword.function'] = { fg = colors.keyword }, -- fn
+  ['@keyword.function'] = { fg = colors.keyword }, -- fn, func
   ['@keyword.operator'] = { fg = colors.keyword },
   ['@keyword.return'] = { fg = colors.keyword },
   ['@keyword.storage'] = { fg = colors.keyword }, -- const, pub, var
-  ['@label'] = { fg = colors.property },
-  ['@method'] = { fg = colors.function_call },
-  ['@method.call'] = { fg = colors.function_call },
+  ['@label'] = { fg = colors.field },
+  ['@method'] = { fg = colors.function_name },
+  ['@method.call'] = { fg = colors.function_name },
   ['@namespace'] = { fg = colors.namespace },
   ['@none'] = { fg = colors.fg },
   ['@number'] = { fg = colors.number },
   ['@operator'] = { fg = colors.operator },
-  ['@parameter'] = { fg = colors.property },
-  ['@parameter.reference'] = { fg = colors.property },
+  ['@parameter'] = { fg = colors.variable }, -- Function parameters should be black
+  ['@parameter.reference'] = { fg = colors.variable },
   ['@preproc'] = { fg = colors.keyword },
-  ['@property'] = { fg = colors.property },
+  ['@property'] = { fg = colors.field }, -- Object properties/keys
   ['@punctuation.bracket'] = { fg = colors.fg },
   ['@punctuation.delimiter'] = { fg = colors.fg },
   ['@punctuation.special'] = { fg = colors.fg },
@@ -224,9 +225,9 @@ local highlights = {
   ['@string.escape'] = { fg = colors.string },
   ['@string.regex'] = { fg = colors.string },
   ['@string.special'] = { fg = colors.string },
-  ['@symbol'] = { fg = colors.property },
+  ['@symbol'] = { fg = colors.field },
   ['@tag'] = { fg = colors.keyword },
-  ['@tag.attribute'] = { fg = colors.property },
+  ['@tag.attribute'] = { fg = colors.field },
   ['@tag.delimiter'] = { fg = colors.fg },
   ['@text'] = { fg = colors.fg },
   ['@text.danger'] = { fg = colors.error },
@@ -246,24 +247,40 @@ local highlights = {
   ['@type.builtin'] = { fg = colors.type_color },
   ['@type.definition'] = { fg = colors.type_color },
   ['@type.qualifier'] = { fg = colors.keyword },
-  ['@variable'] = { fg = colors.property }, -- Variable names
-  ['@variable.builtin'] = { fg = colors.property },
-  ['@variable.member'] = { fg = colors.property }, -- Struct fields
-  ['@variable.parameter'] = { fg = colors.property },
+  ['@variable'] = { fg = colors.variable }, -- Variables should be black
+  ['@variable.builtin'] = { fg = colors.variable },
+  ['@variable.member'] = { fg = colors.variable }, -- Member access should be black
+  ['@variable.parameter'] = { fg = colors.variable },
 
   -- Language-specific highlights
-  -- Zig
+  -- Go specific
+  ['@field.go'] = { fg = colors.field }, -- Struct field definitions
+  ['@property.go'] = { fg = colors.field }, -- Struct literal keys (UserID:, Email:)
+  ['@variable.go'] = { fg = colors.variable }, -- Variables should be black
+  ['@parameter.go'] = { fg = colors.variable }, -- Parameters should be black
+  ['@namespace.go'] = { fg = colors.namespace }, -- Package names (time, jwt)
+  ['@type.go'] = { fg = colors.type_color }, -- Types (string, error)
+  ['@constant.builtin.go'] = { fg = colors.type_color }, -- nil
+  ['@function.go'] = { fg = colors.function_name },
+  ['@method.go'] = { fg = colors.function_name },
+  ['@method.call.go'] = { fg = colors.function_name },
+
+  -- Zig specific
   ['@type.zig'] = { fg = colors.type_color },
-  ['@constant.zig'] = { fg = colors.constant },
-  ['@field.zig'] = { fg = colors.property },
-  ['@property.zig'] = { fg = colors.property },
-  ['@variable.member.zig'] = { fg = colors.property },
+  ['@constant.zig'] = { fg = colors.constant }, -- UPPERCASE constants
+  ['@field.zig'] = { fg = colors.field }, -- Enum members
+  ['@property.zig'] = { fg = colors.field }, -- Struct fields
+  ['@variable.member.zig'] = { fg = colors.field }, -- Enum values like void_pending
+  ['@variable.zig'] = { fg = colors.variable },
+  ['@parameter.zig'] = { fg = colors.variable },
 
   -- Rust
   ['@type.rust'] = { fg = colors.type_color },
   ['@constant.rust'] = { fg = colors.constant },
-  ['@field.rust'] = { fg = colors.property },
-  ['@property.rust'] = { fg = colors.property },
+  ['@field.rust'] = { fg = colors.field },
+  ['@property.rust'] = { fg = colors.field },
+  ['@variable.rust'] = { fg = colors.variable },
+  ['@parameter.rust'] = { fg = colors.variable },
 
   -- TypeScript/JavaScript
   ['@type.typescript'] = { fg = colors.type_color },
@@ -272,24 +289,31 @@ local highlights = {
   ['@constructor.typescript'] = { fg = colors.type_color },
   ['@constant.javascript'] = { fg = colors.constant },
   ['@constant.typescript'] = { fg = colors.constant },
+  ['@property.javascript'] = { fg = colors.field },
+  ['@property.typescript'] = { fg = colors.field },
+  ['@variable.javascript'] = { fg = colors.variable },
+  ['@variable.typescript'] = { fg = colors.variable },
+  ['@parameter.javascript'] = { fg = colors.variable },
+  ['@parameter.typescript'] = { fg = colors.variable },
 
   -- Python
   ['@type.python'] = { fg = colors.type_color },
   ['@constant.python'] = { fg = colors.constant },
-  ['@field.python'] = { fg = colors.property },
-
-  -- Go
-  ['@type.go'] = { fg = colors.type_color },
-  ['@constant.go'] = { fg = colors.constant },
-  ['@field.go'] = { fg = colors.property },
+  ['@field.python'] = { fg = colors.field },
+  ['@variable.python'] = { fg = colors.variable },
+  ['@parameter.python'] = { fg = colors.variable },
 
   -- C/C++
   ['@type.c'] = { fg = colors.type_color },
   ['@type.cpp'] = { fg = colors.type_color },
   ['@constant.c'] = { fg = colors.constant },
   ['@constant.cpp'] = { fg = colors.constant },
-  ['@field.c'] = { fg = colors.property },
-  ['@field.cpp'] = { fg = colors.property },
+  ['@field.c'] = { fg = colors.field },
+  ['@field.cpp'] = { fg = colors.field },
+  ['@variable.c'] = { fg = colors.variable },
+  ['@variable.cpp'] = { fg = colors.variable },
+  ['@parameter.c'] = { fg = colors.variable },
+  ['@parameter.cpp'] = { fg = colors.variable },
 
   -- Git signs
   GitSignsAdd = { fg = colors.git_add },
@@ -326,9 +350,29 @@ local highlights = {
   NvimTreeGitDeleted = { fg = colors.git_delete },
   NvimTreeSpecialFile = { fg = colors.type_color },
   NvimTreeIndentMarker = { fg = colors.border },
-  NvimTreeImageFile = { fg = colors.property },
+  NvimTreeImageFile = { fg = colors.field },
   NvimTreeSymlink = { fg = colors.keyword },
   NvimTreeExecFile = { fg = colors.string },
+
+  -- Neo-tree
+  NeoTreeNormal = { fg = colors.fg, bg = colors.bg },
+  NeoTreeNormalNC = { fg = colors.fg, bg = colors.bg },
+  NeoTreeDirectoryName = { fg = colors.fg },
+  NeoTreeDirectoryIcon = { fg = colors.keyword },
+  NeoTreeRootName = { fg = colors.keyword, bold = true },
+  NeoTreeFileName = { fg = colors.fg },
+  NeoTreeFileIcon = { fg = colors.fg },
+  NeoTreeFileNameOpened = { fg = colors.keyword },
+  NeoTreeSymbolicLinkTarget = { fg = colors.keyword },
+  NeoTreeIndentMarker = { fg = colors.border },
+  NeoTreeGitAdded = { fg = colors.git_add },
+  NeoTreeGitModified = { fg = colors.git_change },
+  NeoTreeGitDeleted = { fg = colors.git_delete },
+  NeoTreeGitUntracked = { fg = colors.git_add },
+  NeoTreeGitIgnored = { fg = colors.fg_tertiary },
+  NeoTreeGitStaged = { fg = colors.git_add },
+  NeoTreeGitUnstaged = { fg = colors.git_change },
+  NeoTreeGitConflict = { fg = colors.error },
 
   -- Indent Blankline
   IndentBlanklineChar = { fg = colors.border },
@@ -336,35 +380,35 @@ local highlights = {
 
   -- Leap.nvim
   LeapMatch = { fg = colors.bg, bg = colors.keyword, bold = true },
-  LeapLabelPrimary = { fg = colors.bg, bg = colors.property, bold = true },
+  LeapLabelPrimary = { fg = colors.bg, bg = colors.field, bold = true },
   LeapLabelSecondary = { fg = colors.bg, bg = colors.type_color, bold = true },
 
   -- Cmp
   CmpItemAbbrMatch = { fg = colors.keyword, bold = true },
   CmpItemAbbrMatchFuzzy = { fg = colors.keyword, bold = true },
   CmpItemKindText = { fg = colors.string },
-  CmpItemKindMethod = { fg = colors.function_call },
-  CmpItemKindFunction = { fg = colors.function_call },
+  CmpItemKindMethod = { fg = colors.function_name },
+  CmpItemKindFunction = { fg = colors.function_name },
   CmpItemKindConstructor = { fg = colors.type_color },
-  CmpItemKindField = { fg = colors.property },
-  CmpItemKindVariable = { fg = colors.property },
+  CmpItemKindField = { fg = colors.field },
+  CmpItemKindVariable = { fg = colors.variable },
   CmpItemKindClass = { fg = colors.type_color },
   CmpItemKindInterface = { fg = colors.type_color },
   CmpItemKindModule = { fg = colors.namespace },
-  CmpItemKindProperty = { fg = colors.property },
+  CmpItemKindProperty = { fg = colors.field },
   CmpItemKindUnit = { fg = colors.number },
   CmpItemKindValue = { fg = colors.number },
   CmpItemKindEnum = { fg = colors.type_color },
   CmpItemKindKeyword = { fg = colors.keyword },
   CmpItemKindSnippet = { fg = colors.string },
-  CmpItemKindColor = { fg = colors.property },
+  CmpItemKindColor = { fg = colors.field },
   CmpItemKindFile = { fg = colors.fg },
-  CmpItemKindReference = { fg = colors.property },
+  CmpItemKindReference = { fg = colors.variable },
   CmpItemKindFolder = { fg = colors.keyword },
-  CmpItemKindEnumMember = { fg = colors.property },
+  CmpItemKindEnumMember = { fg = colors.field },
   CmpItemKindConstant = { fg = colors.constant },
   CmpItemKindStruct = { fg = colors.type_color },
-  CmpItemKindEvent = { fg = colors.property },
+  CmpItemKindEvent = { fg = colors.field },
   CmpItemKindOperator = { fg = colors.operator },
   CmpItemKindTypeParameter = { fg = colors.type_color },
 }
